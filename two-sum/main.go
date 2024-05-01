6ae42339acdc653cb59f460eae99cbe82c80d097
Samuel Camilo <itscamilo@outlook.com>
2024-05-01 00:11:09 -0300

    feat: added optimized solution

--- two-sum/main.go
+++ two-sum/main.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 )
 
-// Brute Force
-func twoSum(nums []int, target int) []int {
+// Brute Force...
+func twoSumBruteForce(nums []int, target int) []int {
 	for i, left := range nums {
 		for j, right := range nums {
 			if left+right == target && i != j {
@@ -17,11 +17,26 @@ func twoSum(nums []int, target int) []int {
 	return nil
 }
 
+func twoSumOptimized(nums []int, target int) []int {
+	hm := make(map[int]int)
+
+	for i, num := range nums {
+		_, ok := hm[num]
+		if ok {
+			return []int{hm[num], i}
+		}
+
+		hm[target-num] = i
+	}
+
+	return nil
+}
+
 func main() {
 	input := []int{2, 7, 11, 15}
 	target := 26
 
-	output := twoSum(input, target)
+	output := twoSumOptimized(input, target)
 
 	fmt.Println(output)
 }
